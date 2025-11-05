ff1e5f1a37c5e92d596692514ea91662412b150c
brib-octo <brice.boutamdja@octo.com>
2025-11-05 11:35:36 +0100

    feat: ajout aria-label pour l'accessibilité lecteur d'écran

--- app/autherror/MigrationJeunePage.tsx
+++ app/autherror/MigrationJeunePage.tsx
@@ -55,8 +55,9 @@ function MigrationJeunePage({
         <Link
           href={`mailto:support@pass-emploi.beta.gouv.fr?subject=Demande%20de%20suppression%20des%20donn%C3%A9es%20personnelles%20%E2%80%93%20${nom}%20${prenom}&body=Bonjour%2C%0A%0AJe%20souhaite%20exercer%20mon%20droit%20%C3%A0%20la%20suppression%20de%20mes%20donn%C3%A9es%20personnelles%20conform%C3%A9ment%20%C3%A0%20l%E2%80%99article%2017.1%20du%20RGPD.%20Vous%20trouverez%20ci%E2%80%91dessous%20les%20informations%20me%20concernant%20afin%20que%20vous%20puissiez%20localiser%20rapidement%20mon%20dossier.%20%0A%0ANom%20%3A%20${nom}%0APr%C3%A9nom%20%3A%20${prenom}%0AAdresse%20e%E2%80%91mail%20utilis%C3%A9e%20dans%20l%E2%80%99application%20%3A%20${email}`}
           className='underline hover:text-primary'
+          aria-label='Envoyer un email pour demander la suppression de vos données'
         >
-          {'ici'}
+          ici
         </Link>
       </p>
     </ErrorPageLayout>
